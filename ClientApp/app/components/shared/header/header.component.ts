f5094433c42a566e87bb1741cba52449804f3edb
Mateusz Tomanek <mateusz.tomanek@gmail.com>
2017-05-03 01:10:59 +0200

    Wieczorny commit

---
+++ ClientApp/app/components/shared/header/header.component.ts
@@ -0,0 +1,14 @@
+﻿
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'shared-element',
+    templateUrl: '/header.component.html'
+})
+export class HeaderComponent {
+    name: string;
+
+    constructor() {
+        this.name = 'Sam';
+    }
+}
